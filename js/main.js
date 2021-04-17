489f71c46f5a273299eb8f21788e6bcdd451547e
Juan-calero <hello.juancalero@gmail.com>
2021-04-17 17:02:43 +0100

    Array Cardio #2

--- js/main.js
+++ js/main.js
@@ -77,3 +77,40 @@
           return obj},{});
           
       console.log(reduceData);
+
+          // ## Array Cardio Day 2
+      const people2 = [
+        { name: 'Wes', year: 1988 },
+        { name: 'Kait', year: 1986 },
+        { name: 'Irv', year: 1970 },
+        { name: 'Lux', year: 2015 }
+      ];
+  
+      const comments = [
+        { text: 'Love this!', id: 523423 },
+        { text: 'Super good', id: 823423 },
+        { text: 'You are the best', id: 2039842 },
+        { text: 'Ramen is my fav food ever', id: 123523 },
+        { text: 'Nice Nice Nice!', id: 542328 }
+      ];
+  
+      // Some and Every Checks
+      // Array.prototype.some() // is at least one person 19 or older?
+      const now = new Date().getFullYear();
+      const older = people2.some(age => now - age.year >= 19);
+      console.log(older)
+      // Array.prototype.every() // is everyone 19 or older?
+      const everyOld = people2.every(age => now - age.year >= 19);
+      console.log(everyOld)
+      // Array.prototype.find()
+      // Find is like filter, but instead returns just the one you are looking for
+      // find the comment with the ID of 823423
+      const searchID = comments.find(a => a.id === 823423);
+
+      console.log(searchID);
+      // Array.prototype.findIndex()
+      // Find the comment with this ID
+      // delete the comment with the ID of 823423
+      const searchIDIndex = comments.findIndex(a => a.id === 823423);
+      comments.splice(searchIDIndex,1)
+      console.table(comments);
